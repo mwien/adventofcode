e3baabc3d40ab4990d8f085891a148055c705ceb
Marcel Wienöbst <marcel7698@gmx.de>
2023-12-10 23:38:44 +0100

    refactor and add part 2

--- 10/10.ex
+++ 10/10.ex
@@ -16,11 +16,17 @@ defmodule AOCDay10 do
     for x <- [-1, 0, 1], y <- [-1, 0, 1], rem(x+y, 2) != 0, do: {i+x, j+y}
   end
 
-  def connection_between({{a1, b1}, c1}, {{a2, b2}, c2}) do
-    if Enum.member?(Enum.map(connections(c1), fn {x, y} -> {a1+x,b1+y} end),  {a2, b2}) and Enum.member?(Enum.map(connections(c2), fn {x, y} -> {a2+x,b2+y} end),  {a1, b1}) do
-      true
-    else 
-      false
+  def connection_fromto({a1, b1}, {a2, b2}, c) do
+    connections(c) 
+    |> Enum.map(fn {x, y} -> {a1+x,b1+y} end) 
+    |> Enum.member?({a2, b2})
+  end
+
+  def connection_between(grid, p1, p2) do
+    cond do
+      !connection_fromto(p1, p2, grid[p1]) -> false
+      !connection_fromto(p2, p1, grid[p2]) -> false 
+      true -> true
     end
   end
 
@@ -37,56 +43,62 @@ defmodule AOCDay10 do
     end)
   end
 
-  def find_tile(grid, {i,j}) do
+  def find_tile(grid, pos) do
     [?|, ?-, ?L, ?J, ?7, ?F]
     |> Enum.filter(fn c -> 
-      length(Enum.filter(neighbors({i,j}), fn {a,b} -> 
-        connection_between({{i, j}, c}, {{a, b}, Map.get(grid, {a,b}, ?.)})
-      end)) == 2 
+      neighbors(pos)
+      |> Enum.filter(&connection_between(Map.put(grid, pos, c), pos, &1))
+      |> length() == 2
     end)
     |> List.first()
   end
 
-  def get_distances(grid, queue, dists) do
-    case :queue.out(queue) do
-      {{:value, {{i, j}, d}}, queue} -> 
-        new_neighbors = Enum.filter(neighbors({i,j}), fn pos -> !Map.has_key?(dists, pos) and Map.has_key?(grid, pos) and connection_between({pos, Map.fetch!(grid, pos)}, {{i, j}, Map.fetch!(grid, {i,j})}) end)
-        new_queue = Enum.reduce(new_neighbors, queue, &:queue.in({&1, d+1}, &2))
-        new_dists = Enum.reduce(new_neighbors, dists, &Map.put(&2, &1, d+1))
-        get_distances(grid, new_queue, new_dists)
-      _ -> dists
+  def traverse(circle = [last | tail], grid, start) do
+    next = neighbors(last)
+    |> Enum.filter(&Map.has_key?(grid, &1))
+    |> Enum.filter(&connection_between(grid, last, &1))
+    |> Enum.filter(&(List.first(tail) != &1))
+    |> List.first()
+    cond do
+      next == start -> circle
+      true -> traverse([next | circle], grid, start)
     end
   end
 
-  def solve(file) do
+  def get_cycle(file) do
     grid = File.read!(file)
     |> to_index_map()
-    |> IO.inspect()
     
-    {i,j} = grid 
+    pos = grid 
     |> Enum.find(fn {_, v} -> v == ?S end)
     |> elem(0)
-    #|> IO.inspect()
 
-    c = find_tile(grid, {i,j}) 
-    queue = :queue.new()
-    queue = :queue.in({{i,j}, 0}, queue)
-
-    dists = Map.put(grid, {i,j}, c)
-    |> get_distances(queue, Map.new([{{i,j}, 0}]))
+    c = find_tile(grid, pos) 
+    grid = Map.put(grid, pos, c) 
     
-    dists
-    |> Map.values()
-    |> Enum.max()
-    |> IO.puts()
-
-    Map.keys(grid)
-    |> Enum.filter(&inside(dists, grid, &1))
-    |> Enum.map(fn {x,y} -> {y+1, x+1} end)
-    |> IO.inspect()
-    |> Enum.count()
-
+    traverse([pos], grid, pos) 
   end
 
+  def part1(file) do
+    get_cycle(file)
+    |> length()
+    |> div(2)
+  end
+
+  def shoelace([_]), do: 0
+
+  def shoelace([{x1, y1}, {x2, y2} | tail]) do
+    (y1 + y2) * (x1 - x2) + shoelace([{x2, y2} | tail])
+  end
+
+  def picks(a, b) do
+    a - div(b, 2) + 1
+  end
+
+  def part2(file) do
+    cycle = get_cycle(file)
+    |> then(&([List.last(&1) | &1]))    
+    picks(div(shoelace(cycle), 2), length(cycle) - 1)
+  end
 end
 
