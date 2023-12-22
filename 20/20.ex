bc6b47a68a4e58fc3e483b93c66eebb964e761a7
Marcel Wienöbst <marcel7698@gmx.de>
2023-12-22 12:49:26 +0100

    day 20 part 1

---
+++ 20/20.ex
@@ -0,0 +1,100 @@
+defmodule AOCDay20 do
+
+  def parse_module(s) do
+    [label, children] = String.split(s, "->")
+    {type, name} = case String.trim(label) do
+      "broadcaster" -> {"B", "broadcaster"}
+      "%" <> rest -> {"%", rest}
+      "&" <> rest -> {"&", rest}
+    end
+
+    children = String.trim(children)
+    |> String.split(", ")
+
+    {name, %{type: type, children: children, parents: [], state: (if type == "&", do: %{}, else: 0)}}
+  end
+
+  def parse_modules(s) do
+    modules = String.split(s, "\n", trim: true) 
+    |> Enum.map(&parse_module(&1))
+    |> Map.new()
+
+    IO.inspect(modules)
+
+    modules = Enum.reduce(Map.keys(modules), modules, fn mod, modules -> 
+      Enum.reduce(modules[mod].children, modules, fn child, modules ->
+        if Map.has_key?(modules, child) do 
+          vals = modules[child] 
+          parents = vals.parents
+          Map.put(modules, child, Map.put(vals, :parents, [mod | parents]))
+        else 
+          modules
+        end
+      end)
+    end) 
+
+    Enum.reduce(Map.keys(modules), modules, fn mod, modules -> 
+      val = modules[mod]
+      if val.type == "&" do
+        update_in(modules[mod][:state], fn _ -> Map.new(val.parents, fn c -> {c, 0} end) end)
+      else 
+        modules
+      end
+    end)
+  end
+
+  def update_modules(modules, name, pulse, from) do
+    case modules[name].type do
+      "%" -> update_in(modules[name][:state], &(if pulse == 0, do: 1 - &1, else: &1))
+      "&" -> update_in(modules[name][:state][from], fn _ -> pulse end)
+      _ -> modules
+    end
+  end
+  
+  def propagate(queue, children, val, from) do
+    Enum.reduce(children, queue, &:queue.in({&1, val, from}, &2))
+  end
+
+  def send_pulse(modules, queue, name, pulse) do
+    case modules[name].type do
+      "B" -> propagate(queue, modules[name].children, 0, name)
+      "%" -> 
+        case pulse do  
+          0 -> propagate(queue, modules[name].children, modules[name].state, name)
+          1 -> queue
+        end
+      "&" -> propagate(queue, modules[name].children, (if Enum.all?(Map.values(modules[name].state), &(&1 == 1)), do: 0, else: 1), name) 
+    end
+  end
+
+  def simulate(modules, {[], []}, {l, h}) do
+    {modules, {l, h}}
+  end
+
+  def simulate(modules, queue, {l, h}) do
+    {{:value, {name, pulse, from}}, queue} = :queue.out(queue)
+    if Map.has_key?(modules, name) do
+      modules = update_modules(modules, name, pulse, from)
+      queue = send_pulse(modules, queue, name, pulse)
+      simulate(modules, queue, {l + 1 - pulse, h + pulse})
+    else 
+      simulate(modules, queue, {l + 1 - pulse, h + pulse}) 
+    end
+  end
+
+  def part1(file) do
+    modules = File.read!(file)
+    |> parse_modules()
+    #|> IO.inspect()
+
+    {_, {l, h}} = Enum.reduce(1..1000, {modules, {0,0}}, fn _, {modules, counts} -> 
+      simulate(modules, :queue.from_list([{"broadcaster", 0, "button"}]), counts) 
+    end)
+
+    l * h
+  end
+end
+
+
+
+
